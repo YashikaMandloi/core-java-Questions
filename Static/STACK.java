08dbe08154c4c1b385c96c2e94ab5b36394a8f5c
YashikaMandloi <145110078+YashikaMandloi@users.noreply.github.com>
2024-09-29 19:22:26 +0530

    Add files via upload

---
+++ Static/STACK.java
@@ -0,0 +1,12 @@
+package stactic;
+
+public class STACK {
+	    public static int x = 10;
+
+	    public static void main(String[] args) {
+	        System.out.println("x: " + x);
+	        x = 20;
+	        System.out.println("x: " + x);
+	    }
+	}
+
