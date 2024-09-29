cc3a8b15cfd547cba73d576bc7ef4cc5242ca378
YashikaMandloi <145110078+YashikaMandloi@users.noreply.github.com>
2024-09-29 20:32:08 +0530

    Add files via upload

---
+++ var/localvariable.java
@@ -0,0 +1,12 @@
+package local;
+
+public class localvariable {
+	
+	    public static void main(String[] args) {
+	        int x = 10; // local variable
+	        System.out.println("x: " + x);
+	        x = 20;
+	        System.out.println("x: " + x);
+	    }
+	}
+
