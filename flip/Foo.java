dda6dc3f6863533356c86529ab3fb4cff53868cd
YashikaMandloi <145110078+YashikaMandloi@users.noreply.github.com>
2024-09-29 20:12:32 +0530

    Add files via upload

---
+++ flip/Foo.java
@@ -0,0 +1,14 @@
+package flip;
+
+public class Foo {
+	
+	public static void main(String s[]) {
+        Integer i = new Integer(1) + new Integer(2);
+        switch(i) {
+            case 3: System.out.println("three"); break;
+            default: System.out.println("other"); break;
+        }
+    }
+}
+
+
---
+++ flip/Swap.java
@@ -0,0 +1,19 @@
+package flip;
+
+public class Swap {
+	
+	    public static void main(String[] args) {
+	        int a = 5;  
+	        int b = 10; 
+
+	        System.out.println("Before Swap: a = " + a + ", b = " + b);
+
+	        
+	        a = a + b; 
+	        b = a - b; 
+	        a = a - b; 
+	        System.out.println("After Swap: a = " + a + ", b = " + b);
+	    }
+	}
+
+
