30b467913e39319b37bca1747c082d7335980a32
YashikaMandloi <145110078+YashikaMandloi@users.noreply.github.com>
2024-09-29 20:33:44 +0530

    Add files via upload

---
+++ set get/Java.java
@@ -0,0 +1,26 @@
+package com.encap;
+
+public class Java {
+	private String name;
+	private String pass;
+	
+	protected String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	
+	
+}
+
---
+++ set get/Main.java
@@ -0,0 +1,16 @@
+package com.encap;
+
+public class Main {
+	public static void main(String[] args) {
+		Java jb1=new Java();
+		jb1.setName("yashika");
+		jb1.setPass("123");
+		System.out.println(jb1.getName());
+		System.out.println(jb1.getPass());
+		
+		
+	}
+}
+	
+
+	
