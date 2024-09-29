95d76451ec1779ff9675b12d9eaf5b7bdd17ae87
YashikaMandloi <145110078+YashikaMandloi@users.noreply.github.com>
2024-09-29 20:24:15 +0530

    Add files via upload

---
+++ instance/MyClass.java
@@ -0,0 +1,30 @@
+package instance;
+
+public class MyClass {
+	
+	    private int x; // instance variable
+
+	    public MyClass(int val) {
+	        x = val;
+	    }
+
+	    public void printX() {
+	        System.out.println("x: " + x);
+	    }
+
+	    public void changeX(int newVal) {
+	        x = newVal;
+	    }
+
+	    public static void main(String[] args) {
+	        MyClass obj1 = new MyClass(10);
+	        obj1.printX(); // prints "x: 10"
+
+	        obj1.changeX(20);
+	        obj1.printX(); // prints "x: 20"
+
+	        MyClass obj2 = new MyClass(30);
+	        obj2.printX(); // prints "x: 30"
+	    }
+	}
+
